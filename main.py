a17f803469452c9296ed49df3016e24ed48b95d2
1MioSubs <it09022022@gmail.com>
2023-04-10 18:56:49 +0300

    version 0.2

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import easyocr
+from datetime import datetime
 
 
 def text_read_to_foto(file_path):
-    reader = easyocr.Reader(["en"])
-    result = reader.readtext(file_path, detail=0, paragraph=True)
+    reader = easyocr.Reader(["en", "ru", "uk"])
+    result = reader.readtext(file_path, paragraph=True)
 
     return result
 
 
 def main():
-    file_path = input("Путь к вашему файлу: ")
-    #test1 = "img/text.jpg"
+    #file_path = input("Путь к вашему файлу: ")
+    test1 = "img/text.jpg"
+    test3 = "img/text2.jpg"
     #test2 = "img/write_heands.jpg"
-    print(text_read_to_foto(file_path=file_path))
-
+    test11 = float((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds())
+    print(text_read_to_foto(file_path=test3))
+    test22 = float((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds())
+    print(test22 - test11)
 
 if __name__ == "__main__":
     main()
