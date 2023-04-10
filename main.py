d340677a9524ac165e927e0e55e7724ead7e6793
1MioSubs <it09022022@gmail.com>
2023-04-10 18:22:30 +0300

    pars img version 0.1

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import easyocr
+
+
+def text_read_to_foto(file_path):
+    reader = easyocr.Reader(["en"])
+    result = reader.readtext(file_path, detail=0, paragraph=True)
+
+    return result
+
+
+def main():
+    file_path = input("Путь к вашему файлу: ")
+    #test1 = "img/text.jpg"
+    #test2 = "img/write_heands.jpg"
+    print(text_read_to_foto(file_path=file_path))
+
+
+if __name__ == "__main__":
+    main()
